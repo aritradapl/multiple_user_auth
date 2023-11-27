40f61ce2a46bb7b8e1065c3ad9b69ef8db0bbccd
Aritra Dutta <aritra.dutta@digitalaptech.co.in>
2023-11-27 12:16:15 +0530

    Changes

--- app/Http/Controllers/Frontend/Auth/RegisterController.php
+++ app/Http/Controllers/Frontend/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Frontend\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
